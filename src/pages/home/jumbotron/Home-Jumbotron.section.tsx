7dd457631be366622ceba782ba587f8c8cae3d73
YusufSyam <muhyusufsyamsyam@gmail.com>
2025-05-29 22:20:16 +0800

    feat: jumbotron responsive

--- src/pages/home/jumbotron/Home-Jumbotron.section.tsx
+++ src/pages/home/jumbotron/Home-Jumbotron.section.tsx
@@ -17,8 +17,8 @@ const HomeJumbotronSection: React.FC<IHomeJumbotronSection> = ({}) => {
   return (
     <Stack className="gap-20 mb-8">
       <Grid gutter={54} columns={24} className="">
-        <Grid.Col span={8} className="flex">
-          <Stack className="w-full items-end relative">
+        <Grid.Col span={24} sm={8} className="flex">
+          <Stack className="w-full items-center md:items-end relative">
             <div className="w-[290px] rounded-full overflow-hidden z-10">
               <img
                 src={jumbotron4}
@@ -26,14 +26,15 @@ const HomeJumbotronSection: React.FC<IHomeJumbotronSection> = ({}) => {
                 className="w-full h-full object-cover"
               />
             </div>
-            <div className="bg-cream absolute w-[294px] h-[294px] rounded-full right-[-1px] top-[-1px] "></div>
+            <div className="bg-cream absolute w-[294px] h-[294px] rounded-full left-1/2 translate-x-[-51%] top-[-1px] md:hidden block"></div>
+            <div className="bg-cream absolute w-[294px] h-[294px] rounded-full right-[-1px] top-[-1px] md:block hidden "></div>
           </Stack>
         </Grid.Col>
-        <Grid.Col span={16} className="">
+        <Grid.Col span={24} sm={16} className="">
           <Stack className="gap-4 ">
             <Stack className="w-fit">
               <Stack className="gap-0">
-                <Text className="font-quicksand-semibold text-5xl text-white tracking-5 text-left">
+                <Text className="font-quicksand-semibold text-3xl md:text-5xl text-white tracking-5 text-left">
                   HELLO THERE, I'M{" "}
                   <span className="text-cream">YUSUF SYAM</span>
                   {/* hello there, i'm <span className="text-cream">yusuf syam</span> */}
@@ -44,9 +45,9 @@ const HomeJumbotronSection: React.FC<IHomeJumbotronSection> = ({}) => {
                 </Text>
               </Stack>
             </Stack>
-            <Stack className="w-[85%] mt-4">
+            <Stack className="w-full md:w-[85%] mt-4">
               <Group className=" relative">
-                <Text className="text-white text-[17px] text-start py-4  tracking-4">
+                <Text className="text-white text-[17px] text-start py-4 tracking-4">
                   {/* Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis corrupti eius quod? Eius repudiandae optio at excepturi quia voluptas, obcaecati ex assumenda omnis mollitia, doloremque corrupti? */}
                   I am an Information Systems graduate, enthusiastic about
                   everything related to data. Curious, driven, and always eager
